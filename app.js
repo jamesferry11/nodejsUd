17615c349bc0ec17b905c0533ce979590f50e8e3
jamesferry11 <jamesferry11@gmail.com>
2017-02-15 23:58:50 -0700

    Updated 1.0.2
    Arrow functions

--- app.js
+++ app.js
@@ -4,24 +4,37 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const titleOptions = {
+        describe: 'Title of Note',
+        demand: true,
+        alias: 't'
+    };
+
+const bodyOptions = {
+    describe: 'The body of the note',
+    demand: true,
+    alias: 'b'
+};
 const argv = yargs
     .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
         title: {
-            describe: 'Title of Note',
+            describe: 'Title of note',
             demand: true,
-            alias: 't'
+            alias:'t'
         },
-        body: {
-            describe: 'The body of the note';
-            demand: true,
-                alias: 'b'
-        }
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
     })
     .help()
     .argv;
-    var command = argv._[0];
 
-        }
+var command = argv._[0];
 
 if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
