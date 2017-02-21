2af516645e997389f0c8bc312aa3ba7c8c9f73e2
jamesferry11 <jamesferry11@gmail.com>
2017-02-20 23:41:24 -0700

    express web server 1.0.1
    added hbs and partials. created partial headers and footers.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,27 +1,42 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'James',
-        likes: [
-            'Books',
-            'Cities'
-        ]
-    })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear(),
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
-});
-
-app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: 'Unable to handle request'
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
     });
 });
 
 
-app.listen(3000);
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle the user request'
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
