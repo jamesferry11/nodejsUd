e4adcc643bf0d453faa34d2579e305e61b982f42
jamesferry11 <jamesferry11@gmail.com>
2017-02-22 01:28:51 -0700

    Created Heroku server
    Created heroku server first upload. created a new repo directly for the heroku uploads. working correctly. created bash install on windows.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next)=> {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -13,7 +30,7 @@ hbs.registerHelper('getCurrentYear', () => {
 
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
-})
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
@@ -37,6 +54,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
